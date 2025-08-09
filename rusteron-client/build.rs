ea72f339ca5ba4edd8d70b614597845733440858
Mo Imran <mohammed.imran@gsr.io>
2025-08-09 11:39:39 +0100

    issue-19 use older version of linux with older version glibc

--- rusteron-client/build.rs
+++ rusteron-client/build.rs
@@ -406,7 +406,7 @@ fn download_precompiled_binaries(artifacts_dir: &Path) -> Result<(), Box<dyn std
         "default"
     };
 
-    let image = if target_os == "macos" && arch == "x86_64" {
+    let mut image = if target_os == "macos" && arch == "x86_64" {
         "13"
     } else {
         "latest"
@@ -414,6 +414,7 @@ fn download_precompiled_binaries(artifacts_dir: &Path) -> Result<(), Box<dyn std
 
     if target_os == "linux" {
         target_os = "ubuntu".to_string();
+        image = "22.04";
     }
 
     let asset = format!("https://github.com/gsrxyz/rusteron/releases/download/v{version}/artifacts-{target_os}-{image}-{feature}.tar.gz");
